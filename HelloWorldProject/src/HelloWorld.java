61bc09ed467cce82444bb62b4c30252d6719061b
Mrs. Adams <colleen_m_adams@mcpsmd.org>
2019-04-04 13:09:16 -0400

    Initial commit

---
+++ HelloWorldProject/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * TODO: Name
+ * TODO: Date
+ * TODO: Class Period
+ * TODO: Program Description
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//TODO: Implement Program Requirements Here
+	}
+
+}
